1b661aa9a3daf243ea59f8115d901830f7ad4592
Phinehas Enakireru <phinehasenakireru@gmail.com>
2024-08-07 18:02:33 +0100

    remove unused imports in main application test class

--- src/test/java/com/bigdecimal/springbootbackend4nextjs/Springbootbackend4nextjsApplicationTests.java
+++ src/test/java/com/bigdecimal/springbootbackend4nextjs/Springbootbackend4nextjsApplicationTests.java
@@ -1,10 +1,3 @@
 package com.bigdecimal.springbootbackend4nextjs;
 
-import org.junit.platform.suite.api.SelectPackages;
-import org.junit.platform.suite.api.Suite;
-import org.junit.platform.suite.api.SuiteDisplayName;
-
-// @Suite
-// @SuiteDisplayName("End-to-End integration tests")
-// @SelectPackages({ "com.bigdecimal.springbootbackend4nextjs.user" })
 class Springbootbackend4nextjsApplicationTests {}
