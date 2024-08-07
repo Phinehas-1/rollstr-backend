f3126a8cc1bc0ffd4f008a26b3b9e2ccfd5f1bb1
Phinehas Enakireru <phinehasenakireru@gmail.com>
2024-08-07 17:49:49 +0100

    remove run tests as test suite

--- src/test/java/com/bigdecimal/springbootbackend4nextjs/Springbootbackend4nextjsApplicationTests.java
+++ src/test/java/com/bigdecimal/springbootbackend4nextjs/Springbootbackend4nextjsApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.platform.suite.api.SelectPackages;
 import org.junit.platform.suite.api.Suite;
 import org.junit.platform.suite.api.SuiteDisplayName;
 
-@Suite
-@SuiteDisplayName("End-to-End integration tests")
-@SelectPackages({ "com.bigdecimal.springbootbackend4nextjs.user" })
+// @Suite
+// @SuiteDisplayName("End-to-End integration tests")
+// @SelectPackages({ "com.bigdecimal.springbootbackend4nextjs.user" })
 class Springbootbackend4nextjsApplicationTests {}
